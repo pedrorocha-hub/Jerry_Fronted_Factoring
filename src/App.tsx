650654a3435d249e9706b297c7bd4f1784f0b5ec
[dyad] <pedrorocha@myupgrade.ai>
2025-10-14 13:31:28 -0300

    Updated src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -51,8 +51,8 @@ function App() {
           <Route path="/rib" element={<ProtectedRoute><Rib /></ProtectedRoute>} />
           <Route path="/comportamiento-crediticio" element={<ProtectedRoute><ComportamientoCrediticio /></ProtectedRoute>} />
 
-          <Route path="/rib-eeff" element={<ProtectedRoute><RibEeffPage /></ProtectedRoute>} />
-          <Route path="/rib-eeff/nuevo" element={<ProtectedRoute><RibEeffForm /></ProtectedRoute>} />          
+          <Route path="/rib-eeff" element={<ProtectedRoute><RibEeffPage/></ProtectedRoute>}/>
+          <Route path="/rib-eeff/nuevo" element={<ProtectedRoute><RibEeffForm/></ProtectedRoute>}/>          
 
 
           <Route path="/admin/users" element={<ProtectedRoute adminOnly><UserManagement /></ProtectedRoute>} />
