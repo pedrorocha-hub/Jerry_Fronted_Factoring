3750b160178e048e89e1b5ed8c05f48d5aff3553
[dyad] <git@dyad.sh>
2025-10-15 00:57:42 -0300

    [dyad] Fixed report saving error by removing old form and updating routes - wrote 1 file(s), deleted 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -17,8 +17,7 @@ import SentinelPage from './pages/SentinelPage';
 import SentinelCreatePage from './pages/SentinelCreatePage';
 import Upload from './pages/Upload';
 import ReporteTributarioPage from './pages/ReporteTributario';
-import RibReporteTributarioPage from './pages/RibReporteTributario';
-import RibReporteTributarioForm from './pages/RibReporteTributarioForm';
+import RibReporteTributarioPage from './pages/RibReporteTributarioPage';
 import VentasMensualesPage from './pages/VentasMensuales';
 import VentasMensualesForm from './pages/VentasMensualesForm';
 import Rib from './pages/Rib';
@@ -60,8 +59,6 @@ function App() {
           <Route path="/reporte-tributario" element={<ProtectedRoute><ReporteTributarioPage /></ProtectedRoute>} />
           
           <Route path="/rib-reporte-tributario" element={<ProtectedRoute><RibReporteTributarioPage /></ProtectedRoute>} />
-          <Route path="/rib-reporte-tributario/new" element={<ProtectedRoute><RibReporteTributarioForm /></ProtectedRoute>} />
-          <Route path="/rib-reporte-tributario/edit/:id" element={<ProtectedRoute><RibReporteTributarioForm /></ProtectedRoute>} />
 
           <Route path="/ventas-mensuales" element={<ProtectedRoute><VentasMensualesPage /></ProtectedRoute>} />
           <Route path="/ventas-mensuales/new" element={<ProtectedRoute><VentasMensualesForm /></ProtectedRoute>} />
